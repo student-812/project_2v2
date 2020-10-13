f8b3a398dea98059934b3d8d35f705ed178df6de
Dmitriy <dmitriy.ponomarenko2001@gmail.com>
2020-10-13 16:40:58 +0300

    new_comm_proj_2v2

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+//009
+// console.log('arr' + " - object");
+// console.log(4 +"5");
+
+// let incr = 10, 
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2+2 * 2 != '6');
+
+// // &&
+// // ||
+
+// const isCheked = true,
+//       isClose = false;
+      
+// console.log(isCheked && !isClose);
+
+
+
+//008
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hi, ${user}`);
